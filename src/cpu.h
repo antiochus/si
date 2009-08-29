75362b53e5b0bd50c78a176916e2061fb84284e3
Jens Mühlenhoff <j.muehlenhoff@gmx.de>
2009-08-29 17:48:39 +0200

    add copyright and README contents

--- src/cpu.h
+++ src/cpu.h
@@ -1,3 +1,21 @@
+/*
+    This file is part of SI - A primitive, but simple Space Invaders emulator.
+	Copyright 1998 - 2009 Jens Mühlenhoff <j.muehlenhoff@gmx.d>
+
+    SI is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SI is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SI.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 //#define PUSH(b)  memory[SP - 1] = (b); SP--;;;  
 //#define PULL()   memory[SP++];;;;          
 //#define PUSH(b)  memory[SP] = (b); SP--  
